d83f533b1a1b5db45684e66729ece32f06319b16
rokasveitas <rokas.veitas@gmail.com>
2021-07-29 01:06:14 -0400

    Added HaarCliff

---
+++ HaarCliff/clif1.jl
@@ -0,0 +1,412 @@
+
+using LinearAlgebra
+using Plots
+using Random
+using QuantumClifford
+using Serialization
+
+clifset = deserialize("cliffgates")
+
+function ends(s::Stabilizer)
+
+	ns, nq = size(s)
+	ends = zeros(Int, (ns, 2))
+
+	tab = stab_to_gf2(s)
+
+	dens = zeros(Int, (nq, 2))
+
+	for stab = 1:ns
+		for qub = 1:nq
+			if tab[stab, qub] != 0 || tab[stab, qub+nq] != 0
+				ends[stab, 1] = qub
+				dens[qub, 1] += 1
+				break
+			end
+		end
+
+		for qub = nq:-1:1
+			if tab[stab, qub] != 0 || tab[stab, qub+nq] != 0
+				ends[stab, 2] = qub
+				dens[qub, 2] += 1
+				break
+			end
+		end
+	end
+
+	return (ends, dens)
+end
+
+function canon_clip(s::Stabilizer)
+
+	nq,ns = size(s)
+
+	d = ends(s)[2]
+
+	if sum([d[:,j] for j=1:2]) == 2 .* ones(Int, nq)
+		return s
+	end
+
+
+	v = [paul for paul in s]
+
+
+	# first, we kill everthing on the lower-left triangle
+
+	startrow = 1
+
+	for col = 1 : nq
+		if startrow > ns - 1
+			break
+		end
+
+		p1i = startrow  # the row where p1 is
+		p1 = [0b0, 0b0]
+		for _= startrow : ns
+			p1 = [xbit(v[startrow])[col], zbit(v[startrow])[col]]
+			if p1 != [0b0, 0b0]
+				startrow = p1i+1
+				break
+			end
+			insert!(v, startrow, pop!(v))
+		end
+
+		# Either we'll find a second pauli here, or the first pauli ends
+		p2i = 0
+		p2 = [0b0, 0b0]
+		for _=startrow : ns
+			p2 = [xbit(v[startrow])[col], zbit(v[startrow])[col]]
+			if p2 != [0b0, 0b0] && p2 != p1
+				p2i = startrow
+				startrow = p2i+1
+				break
+			end
+			insert!(v, startrow, pop!(v))
+		end
+
+		# at this point, we can tell if the first pauli ends because p2==[0b0,0b0]
+
+		# Go down the line and fix the stabilizers whose first qubits are now non-identity
+		if [p1, p2] != [[0b0, 0b0], [0b0, 0b0]]
+			for row = startrow:ns
+
+				prow = [xbit(v[row])[col], zbit(v[row])[col]]
+
+				if prow == p1
+					v[row] = v[row] * v[p1i]
+				elseif prow == p2 && p2 != [0b0, 0b0]
+					v[row] = v[row] * v[p2i]
+				elseif prow != [0b0, 0b0] && p2i != 0
+					v[row] = v[row] * v[p1i] * v[p2i]
+				end
+			end
+		end
+	end
+
+	startrow = ns
+	usedinds = Set{Int}()
+
+	for col = nq:-1:1
+
+		p1 = [0b0, 0b0]
+		p1i = 0
+
+		p2 = [0b0, 0b0]
+		p2i = 0
+		for row = startrow:-1:1
+
+			prow = [xbit(v[row])[col], zbit(v[row])[col]]
+			if prow != [0b0, 0b0]
+				if p1i == 0 && !in(row, usedinds)
+					p1i = row
+					p1 = prow
+					push!(usedinds, p1i)
+				elseif p1 == prow
+					v[row] = v[p1i] * v[row]
+				elseif p2i == 0 && !in(row, usedinds)
+					p2i = row
+					p2 = prow
+					push!(usedinds, p2i)
+				elseif p2 == prow
+					v[row] = v[p2i] * v[row]
+				elseif p1i != 0 && p2i != 0
+					v[row] = v[p1i] * v[p2i] * v[row]
+				end
+			end
+		end
+	end
+
+
+	sout = Stabilizer(v)
+
+	return sout
+end
+
+function ent(s::Stabilizer, fir::Int, las::Int; canon=true)
+
+	nq, ns = size(s)
+
+	d = ends(s)[2]
+
+	if canon && sum([d[:,j] for j=1:2]) != 2 .* ones(Int, nq)
+		s = canon_clip(s)
+	end
+
+	es = ends(s)[1]
+
+	crossings = 0
+
+	for stabi = 1:size(es)[1]
+		e = es[stabi,:]
+		if e[1] < fir && e[2] <= las && e[2] >= fir
+			crossings += 1
+			#println(stabi)
+		elseif e[2] > las && e[1] >= fir && e[1] <= las
+			crossings += 1
+			#println(stabi)
+		end
+	end
+
+	return crossings/2
+end
+
+function colpermute(s::Stabilizer, perm)
+	r = copy(s)
+	return colpermute!(r, perm)
+end
+
+
+function mutual_inf(s::Stabilizer, A::Union{Vector{Int}, Int}, B::Union{Vector{Int}, Int})
+
+	if typeof(A) <: Int
+		A = [A]
+	end 
+
+	if typeof(B) <: Int
+		B = [B]
+	end
+
+	if A == B
+		return 0
+	end
+
+	Al = size(A)[1]
+	Bl = size(B)[1]
+	
+	# print("A: ")
+	# println(A)
+	# println(Al)
+	# print("B: ")
+	# println(B)
+	# println(Bl)
+
+	perm = vcat(A, B, [i for i=1:s.nqubits if !in(i, A) && !in(i, B)])
+	r = colpermute(s, perm)
+
+	# println(perm)
+
+	AB = ent(r, 1, Al + Bl)
+	# print("AB: ")
+	# println(AB)
+	A = ent(r, 1, Al)
+	# print("A: ")
+	# println(A)
+	B = ent(r, Al + 1, Al + Bl)
+	# print("B: ")
+	# println(B)
+
+	return A + B - AB
+end
+
+function tim_meas(s::Stabilizer, a::Int, b::Int; print=false)
+
+	sc = copy(s)
+	nq = size(sc)[1]
+
+	zs = one(Stabilizer, nq)
+
+	
+
+	for i=1:nq
+		if i != a && i != b
+			sc, antiind, result = project!(sc, zs[i])
+			if isnothing(result)
+				sc.phases[antiind] = rand([0x0, 0x2])
+			end
+		end
+	end
+
+	sc = canon_clip(sc)
+
+	if print
+		println(sc)
+	end
+
+	if ends(sc)[2][:,1] == ones(nq)
+		return 0
+	end
+	return 1
+
+end
+
+
+function run_brick(N::Int,
+				   depth::Int;
+				   p=0, 
+				   evol=false,
+				   f = x -> 0)
+
+	s = one(Stabilizer, N)
+
+	nq, ns = size(s)
+
+	#ents = []
+
+	bonds = [[[j, j+1] for j=1:2:N-1-N%2],
+			 [[j, j+1] for j=2:2:N-2+N%2]]
+
+	zs = one(Stabilizer, N)
+
+	for d in 1:depth
+
+		for bond in bonds[(d-1)%2+1]
+			apply!(s, rand(clifset), bond)
+		end
+
+		for i=1:N
+			if rand() < p
+				s, antiind, result = project!(s, zs[i])
+				if isnothing(result)
+					s.phases[antiind] = rand([0x0, 0x2])
+				end
+			end
+		end	
+
+		if evol
+			push!(ents, f(s))
+		end
+
+	end
+
+	s = canon_clip(s)
+
+	if !evol
+		ents = f(s)
+	end
+
+	return (s, ents)
+end
+
+
+function avg_S(N::Int,
+			   depth::Int,
+			   it::Int;
+			   p=0, 
+			   evol=false,
+			   f = x -> 0)
+	
+	siz = f(one(Stabilizer, N))
+
+	if typeof(siz) <: Number
+		if evol
+			ents = zeros(depth)
+		else
+			ents = 0
+		end
+	else
+		siz = size(siz)
+		siz = evol ? Tuple(push!([i for i in siz], depth)) : siz
+		ents = zeros(siz)
+	end
+
+	# println("siz: ")
+	# println(siz)
+	# println("ents: ")
+	# println(ents)
+
+	for i=1:it
+		if typeof(ents) <: Number
+			ents += run_brick(N, depth, p=p, evol=evol, f=f)[2]
+		else
+			ents .+= run_brick(N, depth, p=p, evol=evol, f=f)[2]
+		end
+
+		# if i%100 == 0
+		# 	println(i)
+		# end
+	end
+
+	if typeof(ents) <: Number
+		ents /= it
+	else
+		ents ./= it
+	end
+
+	return ents
+end
+
+function find_pow(x::Vector, y::Vector)
+	lx = copy(x)
+	ly = copy(y)
+
+	n = length(x)
+	for i in 1:n
+		if ly[i] == 0
+			lx[i] = 0
+		end
+
+		if lx[i] == 0
+			ly[i] = 0
+		end
+	end
+
+	filter!(a -> a!=0, lx)
+	filter!(a -> a!=0, ly)
+
+	println(lx)
+	println(ly)
+
+	n = length(lx)
+
+	if length(ly) != n
+		throw("bad dims")
+	end
+
+	lx = log.(lx)
+	ly = log.(ly)
+
+	beta = (n * dot(lx, ly) - sum(lx) * sum(ly)) / (n * dot(lx, lx) - sum(lx)^2)
+	alph = (sum(ly) - beta * sum(lx)) / n
+
+	return (pow=beta, coef=exp(alph), n=n)
+end
+
+function avg_mut(N::Int,
+				 depth::Int,
+				 it::Int,
+				 p=0)
+	m = zeros(N-1)
+	for i=1:it
+		s = run_brick(N, depth, p, false)
+		if !(typeof(s) <: Stabilizer)
+			println(typeof(s))
+			throw("oh noooo")
+		end
+		for q=2:N-1
+			m[q] += mutual_inf(s, 1, q)
+		end
+		if i%100 == 0
+			println(i)
+		end
+	end
+
+	m ./= it
+
+	return m
+end
+
+
+
+
+
+
---
+++ HaarCliff/haar1.jl
@@ -0,0 +1,263 @@
+
+using LinearAlgebra
+using Plots
+using Random
+using Statistics
+using ITensors
+using PastaQ
+import PastaQ: gate, randomlayer
+
+# Stolen from Stef: 
+# First attempt to calculate mutual information based on Renyi-2
+# Based on two copies of the system
+function mutual_inf(psi0::MPS, aval::Int, bmax::Int, M::Int)
+	psi = normalize!(copy(psi0))
+	N = length(psi)
+
+    # array to store information
+	I = zeros(4)
+
+    # draw samples (four sets since we want I_A, I_B and I_{A,B})
+	samps_1 = getsamples(psi, M, local_basis=["Z"])
+	samps_2 = getsamples(psi, M, local_basis=["Z"])
+	samp_data_1 = PastaQ.convertdatapoints(copy(samps_1); state=true)
+	samp_data_2 = PastaQ.convertdatapoints(copy(samps_2); state=true)
+
+    # evaluate entropies
+	a = [aval]
+	for (count, bval) in enumerate(aval+9:3:bmax)
+		b = [bval]
+
+		ab = zeros(2)
+		ab[1] = aval
+		ab[2] = bval
+
+        # switch subsets in copies of system
+		samp_data_a1_switch = copy(samp_data_1)
+		samp_data_a2_switch = copy(samp_data_2)
+		samp_data_b1_switch = copy(samp_data_1)
+		samp_data_b2_switch = copy(samp_data_2)
+		samp_data_ab1_switch = copy(samp_data_1)
+		samp_data_ab2_switch = copy(samp_data_2)
+
+		s = siteinds(psi)
+
+		trace_rho_a = 0.
+		trace_rho_b = 0.
+		trace_rho_ab = 0.
+
+        # get traces of density matrices from all samples
+		for m in 1:M
+			x1 = samp_data_1[m,:]
+			x2 = samp_data_2[m,:]
+
+			for n in a
+				samp_data_a1_switch[m,Int(n)] = samp_data_2[m,Int(n)]
+				samp_data_a2_switch[m,Int(n)] = samp_data_1[m,Int(n)]	
+			end
+
+			for n in b
+				samp_data_b1_switch[m,Int(n)] = samp_data_2[m,Int(n)]
+				samp_data_b2_switch[m,Int(n)] = samp_data_1[m,Int(n)]
+			end
+
+			for n in ab
+				samp_data_ab1_switch[m,Int(n)] = samp_data_2[m,Int(n)]
+				samp_data_ab2_switch[m,Int(n)] = samp_data_1[m,Int(n)]
+			end
+
+			xa1_switch = samp_data_a1_switch[m,:]
+			xa2_switch = samp_data_a2_switch[m,:]
+			xb1_switch = samp_data_b1_switch[m,:]
+			xb2_switch = samp_data_b2_switch[m,:]
+			xab1_switch = samp_data_ab1_switch[m,:]
+			xab2_switch = samp_data_ab2_switch[m,:]
+
+			psi_1 = dag(psi[1]) * state(x1[1], s[1])
+			psi_2 = dag(psi[1]) * state(x2[1], s[1])
+			psi_a1_switch = dag(psi[1]) * state(xa1_switch[1], s[1])
+			psi_a2_switch = dag(psi[1]) * state(xa2_switch[1], s[1])
+			psi_b1_switch = dag(psi[1]) * state(xb1_switch[1], s[1])
+			psi_b2_switch = dag(psi[1]) * state(xb2_switch[1], s[1])
+			psi_ab1_switch = dag(psi[1]) * state(xab1_switch[1], s[1])
+			psi_ab2_switch = dag(psi[1]) * state(xab2_switch[1], s[1])
+
+			for j in 2:N
+				psi_1_r = dag(psi[j]) * state(x1[j], s[j])
+				psi_2_r = dag(psi[j]) * state(x2[j], s[j])
+				psi_a1_switch_r = dag(psi[j]) * state(xa1_switch[j], s[j])
+				psi_a2_switch_r = dag(psi[j]) * state(xa2_switch[j], s[j])
+				psi_b1_switch_r = dag(psi[j]) * state(xb1_switch[j], s[j])
+				psi_b2_switch_r = dag(psi[j]) * state(xb2_switch[j], s[j])
+				psi_ab1_switch_r = dag(psi[j]) * state(xab1_switch[j], s[j])
+				psi_ab2_switch_r = dag(psi[j]) * state(xab2_switch[j], s[j])
+
+				psi_1 = psi_1 * psi_1_r
+				psi_2 = psi_2 * psi_2_r
+				psi_a1_switch = psi_a1_switch * psi_a1_switch_r
+				psi_a2_switch = psi_a2_switch * psi_a2_switch_r
+				psi_b1_switch = psi_b1_switch * psi_b1_switch_r
+				psi_b2_switch = psi_b2_switch * psi_b2_switch_r
+				psi_ab1_switch = psi_ab1_switch * psi_ab1_switch_r
+				psi_ab2_switch = psi_ab2_switch * psi_ab2_switch_r
+			end
+
+			trace_rho_a += (psi_a1_switch[] / psi_1[]) * (psi_a2_switch[] / psi_2[])
+			trace_rho_b += (psi_b1_switch[] / psi_1[]) * (psi_b2_switch[] / psi_2[])
+			trace_rho_ab += (psi_ab1_switch[] / psi_1[]) * (psi_ab2_switch[] / psi_2[])
+		end
+
+		trace_rho_a /= M
+		trace_rho_b /= M
+		trace_rho_ab /= M
+
+		entropy_a = - real(log2(trace_rho_a))
+		entropy_b = - real(log2(trace_rho_b))
+		entropy_ab = - real(log2(trace_rho_ab))
+
+		I[count] = entropy_a + entropy_b - entropy_ab
+
+		println(ab)
+	end
+
+	return I
+end
+
+# no longer stolen from Stef
+
+gate(::GateName"Π0") = [ 1 0
+					     0 0 ]
+
+gate(::GateName"Π1") = [ 0 0
+						 0 1 ]
+
+function randomlayer(
+  gatename::AbstractString,
+  support::Union{Vector{<:Int},Vector{Tuple},AbstractRange};
+  rng=Random.GLOBAL_RNG,
+  kwargs...,
+)
+  layer = []
+  for n in support
+    pars = randomparams(gatename, 4; rng=rng) # 4 is because it's a 4x4 matrix
+    gatepars = (
+      if isempty(pars)
+        (isempty(kwargs) ? nothing : values(kwargs))
+      else
+        merge(pars, values(kwargs))
+      end
+    )
+    g = (isnothing(gatepars) ? (gatename, n) : (gatename, n, gatepars))
+    push!(layer, g)
+  end
+  return layer
+end
+
+function measurement!(ψ0::MPS, site::Int, cutoff::Real, maxdim::Real)
+	ψ = normalize!(copy(ψ0))
+
+	orthogonalize!(ψ, site)
+	ρ = prime(ψ[site], tags="Site") * dag(ψ[site])
+
+	probs = real.(diag(array(ρ)))
+	#print("sum(probs: ")
+	#println(sum(probs))
+
+	σ = Int(rand() > probs[1])
+
+	ψ = runcircuit(ψ,("Π"*"$(σ)",site), svd_alg="recursive", cutoff=cutoff, maxdim=maxdim)
+
+
+	normalize!(ψ)
+	ψ0[:] = ψ
+
+end
+
+function entanglement_entropy(ψ0::MPS, b::Int, N::Int)
+		ψ = normalize!(copy(ψ0))
+
+		orthogonalize!(ψ, b)
+		_, S = svd(ψ[b], (linkind(ψ, b-1), siteind(ψ, b)))
+
+		SvN = 0.0
+		for k = 1:dim(S, 1)
+			p = S[k, k]^2
+			SvN -= p * log(p + 1e-20)
+		end
+
+		return SvN
+end
+
+
+
+
+function run_brick(
+  state::MPS,
+  N::Int,
+  depth::Int,
+  p=0;
+  twoqubitgates::Union{String,Vector{String}}="randU",
+  onequbitgates::Union{Nothing,String,Vector{String}}=nothing,
+  evol::Bool=false,
+  rng=Random.GLOBAL_RNG,
+)
+	ψ = state
+	S = evol ? [] : 0
+
+	bonds = PastaQ.lineararray(N)
+
+	for d in 1:depth
+		
+		layer = []
+
+		# one-qubit gates
+		if !isnothing(onequbitgates)
+			append!(layer, randomlayer(onequbitgates, N; rng=rng))
+		end
+
+		# two-qubit gates
+		append!(layer, randomlayer(twoqubitgates, bonds[2 - d%2]; rng=rng))
+
+		# run unitaries
+		ψ = runcircuit(ψ, layer, svd_alg="recursive")
+
+	
+		# projective measurements
+
+		for b = 1:N
+			if rand() < p
+				measurement!(ψ, b, 1e-10, 10_000)
+			end
+		end
+
+		if evol && d%2 == 1
+			push!(S, entanglement_entropy(ψ, N÷2, N))
+		end
+	end
+
+	if !evol
+		S = entanglement_entropy(ψ, N÷2, N)
+	end
+	#println(S)
+	#println(typeof(S))
+	return (ψ , S)
+end
+
+function run_savg(N::Int, depth::Int, ite::Int, p::Real, evol::Bool)
+	#global savg = zeros(depth)
+	#println(savg)
+	#ents = []
+
+	s_avg = evol ? zeros(depth÷2) : 0
+
+	for i = 1:ite
+		ψ0 = productstate(N)
+		_, S = run_brick(ψ0, N, depth, p, evol=evol)
+		
+		s_avg += S
+	end
+
+	s_avg ./= ite
+	return s_avg
+			
+end
