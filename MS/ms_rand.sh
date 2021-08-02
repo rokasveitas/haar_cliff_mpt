484c6cfc20a0a32254360c1e5f8991e9be7ac6d7
rokasveitas <rokas.veitas@gmail.com>
2021-08-02 14:38:39 -0400

    Added ms_rand.sh

---
+++ MS/ms_rand.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=def-rgmelko
+#SBATCH --mem=124G
+#SBATCH --time=1-00:00:00
+#SBATCH --mail-user=rokas@mit.edu
+#SBATCH --mail-type=ALL
+#SBATCH --array=108-192
+
+module add julia
+julia ~/jstuff/haar_cliff_mpt/MS/ms_rand.jl $SLURM_ARRAY_TASK_ID
