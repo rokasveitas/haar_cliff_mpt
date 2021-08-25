64056591d24b085aa76aedad9d3b8f67e1b180f5
Rokas Veitas <rokas.veitas@gmail.com>
2021-08-24 22:31:51 -0400

    More msdata

--- MS/ms_rand.sh
+++ MS/ms_rand.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=1-00:00:00
 #SBATCH --mail-user=rokas@mit.edu
 #SBATCH --mail-type=ALL
-#SBATCH --array=193-786
+#SBATCH --array=787-1572
 
 module add julia
 julia ~/jstuff/haar_cliff_mpt/MS/ms_rand.jl $SLURM_ARRAY_TASK_ID
